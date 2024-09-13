1b07be92f7d82724516571a387ad143db920d27e
jasnapk-hm <hmjasnapk@gmail.com>
2024-09-13 14:58:41 +0530

    index.js updated

--- Server/index.js
+++ Server/index.js
@@ -2,3 +2,8 @@ import express from "express"
 import mongoose from "mongoose"
 import bodyParser from "body-parser"
 import dotenv from "dotenv"
+
+const app=express();
+
+app.use(bodyParser.json())
+dotenv.config();
